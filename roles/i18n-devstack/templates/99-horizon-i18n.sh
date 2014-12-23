50e018884794ea0c4c540bb9892f8cf953062737
Akihiro Motoki <motoki@da.jp.nec.com>
2014-12-23 16:48:38 +0900

    Add playbook for horizon i18n check site

---
+++ roles/i18n-devstack/templates/99-horizon-i18n.sh
@@ -0,0 +1,34 @@
+# ironic.sh - Devstack extras script to install ironic
+
+if is_service_enabled horizon; then
+    if [[ "$1" == "source" ]]; then
+        :
+    elif [[ "$1" == "stack" && "$2" == "pre-install" ]]; then
+        cd $DEST/horizon
+        rm -rf horizon/locale
+        rm -rf openstack_dashboard/locale
+        git checkout -- .
+    elif [[ "$1" == "stack" && "$2" == "install" ]]; then
+        :
+    elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
+        :
+    elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
+        local_settings=$HORIZON_DIR/openstack_dashboard/local/local_settings.py
+        _horizon_config_set $local_settings OPENSTACK_API_VERSIONS identity 3
+        _horizon_config_set $local_settings OPENSTACK_API_VERSIONS volume 2
+        _horizon_config_set $local_settings OPENSTACK_CINDER_FEATURES enable_backup True
+
+        $HOME/horizon-i18n-tools/import-trans.sh -r {{ branch|basename }}
+
+        restart_apache_server
+        :
+    fi
+
+    if [[ "$1" == "unstack" ]]; then
+        :
+    fi
+
+    if [[ "$1" == "clean" ]]; then
+        :
+    fi
+fi
