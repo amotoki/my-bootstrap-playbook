b26a91c175654874c69a4262be697a7303ef6463
Akihiro Motoki <motoki@da.jp.nec.com>
2015-02-18 16:34:57 +0900

    Add git-clone-openstack-repo.sh

---
+++ roles/myenv/files/bin/git-clone-openstack-repo.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+GIT_BASE=https://git.openstack.org/
+
+usage() {
+  cat <<EOF
+Usage: $0 <repo> [category]
+
+  repo: repository base name
+  category: openstack, openstack-dev, openstack-infra, stackforge
+    If not speciifed, all categories will be tried in this order.
+    As shortcuts, dev, infra, stack|forge|sf can be used.
+EOF
+}
+
+get_category() {
+  local category=$1
+  if [ -n "$category" ]; then
+    case $category in
+      openstack)
+        echo openstack
+        ;;
+      openstack-dev|dev)
+        echo openstack-dev
+        ;;
+      openstack-infra|infra)
+        echo openstack-infra
+        ;;
+      stackforge|stack|forge|sf)
+        echo stackforge
+        ;;
+      *)
+        echo "The specified category is not supported."
+        usage
+        exit 1
+    esac
+  else
+    echo openstack openstack-infra openstack-dev stackforge
+  fi
+}
+
+if [ -z "$1" ]; then
+  usage
+  exit 1
+fi
+repo=$1
+categories=$(get_category $2)
+
+for category in $categories; do
+  git clone $GIT_BASE/$category/$repo
+  if [ $? -eq 0 ]; then
+    break
+  fi
+done
