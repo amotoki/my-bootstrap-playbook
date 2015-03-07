cf3379a93a2151f8c3c6b697b89bba012fef41b7
Akihiro Motoki <motoki@da.jp.nec.com>
2015-03-07 22:58:42 +0900

    Add convenient script run.sh

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -x
+
+ENV=`hostname`
+
+if [ -z "$2" ]; then
+  echo "Usage: $0 <host> <playbook> [playbook options...]"
+  exit 1
+fi
+
+HOST=$1
+shift
+PLAYBOOK=$1
+shift
+
+INVENTORY=inventory/${ENV}/hosts
+if [ ! -f $INVENTORY ]; then
+  echo "Please create inventory for $ENV or run ansible-playbook directly."
+  exit 1
+fi
+
+ansible-playbook -i $INVENTORY -e host=$HOST $PLAYBOOK "$@"
