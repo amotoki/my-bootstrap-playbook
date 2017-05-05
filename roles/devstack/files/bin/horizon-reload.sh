ffd4b5693f8d6068ec67e76b8723a58a53c02f83
Akihiro Motoki <amotoki@gmail.com>
2017-05-06 06:06:20 +0900

    devstack: Update horizon-reload.sh

--- roles/devstack/files/bin/horizon-reload.sh
+++ roles/devstack/files/bin/horizon-reload.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+  echo "$0 (apache|runserver) [nocompress]"
+  exit 1
+fi
+
 cd /opt/stack/horizon
 
-DJANGO_SETTINGS_MODULE=openstack_dashboard.settings python manage.py collectstatic --noinput
-DJANGO_SETTINGS_MODULE=openstack_dashboard.settings python manage.py compress --force
+find horizon -name '*.pyc' | xargs rm
+find openstack_dashboard -name '*.pyc' | xargs rm
 
-sudo service apache2 reload
+DJANGO_SETTINGS_MODULE=openstack_dashboard.settings python manage.py collectstatic --noinput
+if [ "$2" != 'nocompress' ]; then
+  DJANGO_SETTINGS_MODULE=openstack_dashboard.settings python manage.py compress --force
+fi
+
+if [ "$1" = "runserver" ]; then
+  tox -e runserver -- 0.0.0.0:8000
+else
+  sudo service apache2 reload
+fi
